8da4c6fbbcfee1e3406c4a3e80ff8e85732de5de
Bruno Santeramo <bruno.santeramo@gmail.com>
2016-08-03 17:19:34 +0200

    2013-08-03 tag

---
+++ CHANGELOG-MANAX.md
@@ -0,0 +1,8 @@
+# Change Log
+All enhancements and patches to Cookiecutter Django will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+##[2016-08-03]
+## Added
+- SQLite backend support for local environment
+- apps in apps folder
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 __version__ = '{{ cookiecutter.version }}'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
+__manax__ = '2016-08-03'
+__manax_info__ = tuple([int(num) if num.isdigit() else num for num in __manax__.replace('-', '.').split('.')])
